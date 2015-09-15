7023495d345227b944d54d97a3bb0fbe74563cd7
Wilfredo G Gomez <wgome015@fiu.edu>
2015-09-14 20:47:25 -0400

    db credentials

--- Website/verify.php
+++ Website/verify.php
@@ -14,13 +14,12 @@
 <body>
     <?php
     require "functions.php";
-    
+
     if( $_SERVER["REQUEST_METHOD"] == "GET" )
     {
         if( !empty($_GET["type"]) && !empty($_GET["email"]) && !empty($_GET["hash"]) )
         {
             //verify data
-            //echo "Get is giving: type: ".$_GET["type"]."  email: ".$_GET["email"]."   hash: ".$_GET["hash"];
             authenticateEmail( $_GET["type"], $_GET["email"], $_GET["hash"] );
         }
     }
