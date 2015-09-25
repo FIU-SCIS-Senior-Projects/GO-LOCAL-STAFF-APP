2ccd1c9d2e39913b8e0f1ea927e7f8471fc15a27
Wilfredo G Gomez <wgome015@fiu.edu>
2015-09-25 08:31:15 -0400

    bug fixes

--- Website/functions.php
+++ Website/functions.php
@@ -144,7 +144,7 @@ function authenticatePhoneNumber( $peopleID, $to, $smsGateway )
 		//		  VALUES ('".$to."', '".$code."')";
 
 		$query = "UPDATE people
-				  SET phone='".$phone."', phonecode='".$phonecode."'
+				  SET phone='".$phone."', phonecode='".$code."'
 				  WHERE peopleID=".$peopleID."";
 
 		if( !$result = mysqli_query( $db, $query ) )
