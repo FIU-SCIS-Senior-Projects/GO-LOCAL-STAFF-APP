946d0b21edc8d7ffc93608bdcea1e90f04774cea
Wilfredo G Gomez <wilfredoggomez@Wilfredos-MacBook-Pro.local>
2015-10-18 20:55:04 -0400

    Final JSON Employer registration

--- Iphone Application/GoLocalApp/GoLocalApp/EmployerRegistration2ViewController.m
+++ Iphone Application/GoLocalApp/GoLocalApp/EmployerRegistration2ViewController.m
@@ -95,8 +95,6 @@
 {
     if( [self verifyDataEnter] )
     {
-        [self sendData];
-        
         [self performSegueWithIdentifier:@"goToEmployerRegister3" sender:self];
     }
 }
@@ -123,116 +121,6 @@
                                           cancelButtonTitle:cancelTitle
                                           otherButtonTitles: nil];
     [alert show];//display alert
-}//eom
-
-
-
-/****** JSON Methods *******/
-
-//Encapsulates the data in a Dictionary to send to the server
--(NSMutableDictionary *) prepareDataToSend
-{
-    NSMutableDictionary *list = [[NSMutableDictionary alloc] init];
-    
-    
-    list[@"registrationType"] = [registeredEmployer getAccountType];
-    list[@"employerID"] = [registeredEmployer getEmployerID];
-    list[@"employerName"] = [registeredEmployer getEmployerName];
-    list[@"typeOfEmployer"] = [registeredEmployer getTypeOfEmployer];
-    list[@"address"] = [registeredEmployer getAddress];
-    list[@"city"] = [registeredEmployer getCity];
-    list[@"state"] = [registeredEmployer getState];
-    list[@"zipCode"] = [registeredEmployer getZipCode];
-    
-    if( [registeredEmployer hasInsurance] )
-        list[@"hasInsurance"] = @"1";
-    else
-        list[@"hasInsurance"] = @"0";
-    
-    return list;
-}
-
-//Sends data of employer to the server
--(void) sendData
-{
-    NSString *serverAddress = @"http://45.55.208.175/Website/jsonPOST_registration.php";//hard coding website
-    
-    NSMutableDictionary *list = [self prepareDataToSend];
-    NSLog(@"");
-    NSLog(@" about to send the following data: %@", list);
-    NSLog(@"");
-
-    NSMutableURLRequest *request = [[NSMutableURLRequest alloc] initWithURL:[NSURL URLWithString:serverAddress]];
-    
-    [request setHTTPMethod:@"POST"];
-    
-    NSError *error;
-    NSData *data = [NSJSONSerialization dataWithJSONObject:list options:0 error:&error];
-    
-    [request setHTTPBody:data];
-    
-    [NSURLConnection connectionWithRequest:request delegate:self];
-}
-
--(void) processServerResponse:(NSDictionary *) responce
-{
-    //    NSLog(@"[1] responce: %@", responce);
-    
-    NSDictionary * userResults = [responce objectForKey:@"results"];
-    int responceType = [[userResults objectForKey:@"responseType"] intValue];
-    NSDictionary * responceMessage = [userResults objectForKey:@"message"];
-    NSString * message = [NSString stringWithFormat:@"%@", responceMessage];
-    
-    NSLog(@"[1] results is %@", userResults);
-    NSLog(@"[1] responceType is %d", responceType);
-    if(responceType > 0) //responce was good
-    {
-        
-    }
-    else //invalid response
-    {
-        //notifying user code was accepted
-        [self showAlert:@"Information invalid" withMessage:message and:@"Okay"];
-    }
-}//eom
-
-
-
-/****** JSON POST METHODS *******/
-
-/* responce from server */
-- (void) connection:(NSURLConnection *)connection didReceiveResponse:(NSURLResponse *)response
-{
-    NSHTTPURLResponse *httpResponse = (NSHTTPURLResponse *)response;
-    
-    NSLog(@" responce: %@", httpResponse.description);
-    
-    //            NSInteger statusCode = httpResponse.statusCode;
-    //            NSLog(@" status Code: %ld", (long)statusCode);
-    
-    //    NSString *string = [NSString stringWithFormat:@"%ld", (long)statusCode];
-    
-}//eo-action
-
-
-/* data received from server */
-- (void)connection:(NSURLConnection *)connection didReceiveData:(nonnull NSData *)data
-{
-    
-    NSDictionary * rawExhibits = [NSJSONSerialization JSONObjectWithData:data options:kNilOptions error:nil];
-    //        NSLog(@"[1] from server replied: %@",rawExhibits);
-    
-    //        NSString *dataResponce = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
-    //        NSLog(@"[2] responce from server %@",dataResponce);
-    //
-    //        // Get JSON objects into initial array
-    //        NSArray *rawExhibits2 = (NSArray *)[NSJSONSerialization JSONObjectWithData:[dataResponce dataUsingEncoding:NSUTF8StringEncoding] options:0 error:NULL];
-    //        NSLog(@"[3] responce from server %@",rawExhibits2);
-    
-    //processing responce
-    [self processServerResponse:rawExhibits];
-    
-    //        }
 }//eom
 
 
