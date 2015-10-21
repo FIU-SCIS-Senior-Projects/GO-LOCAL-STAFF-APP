3d9d3416c771dda359ed641e4d4566eb41b894bf
luandrecast <luoandre29@yahoo.com>
2015-10-21 17:00:17 -0400

    db connect shell script update

--- Database/connectToDB.sh
+++ Database/connectToDB.sh
@@ -4,9 +4,17 @@
 #displaying password
 echo "connecting to mysql"
 echo "(password is 'fall2015)"
+echo " "
+echo " Notes:"
+echo "    [delete database] drop database golocalapp"
+echo " "
+echo "    [create database] create database golocalapp"
+echo " "
+
 
 #connecting to mysql
 mysql -u root -p
+
 
 
 
