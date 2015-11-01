1c106c58ee5b1368fc98d7e8ae21d44cc422d0f3
luandrecast <luoandre29@yahoo.com>
2015-11-01 15:37:47 -0500

    setting temp JSON Search talent

---
+++ Website/jsonPOST_TESTsearchTalent.php
@@ -0,0 +1,106 @@
+<?php
+
+  //making sure its  valid POST request
+ if( $_SERVER["REQUEST_METHOD"] == "POST" )
+  {
+    //getting php post body data
+    $jsondata = file_get_contents('php://input');
+    if ($jsondata) 
+    {
+      //converting json to php array
+      $decoded = json_decode($jsondata, true);
+        switch (json_last_error())
+        {
+          case JSON_ERROR_NONE:
+              $jsonError = ' - No errors';
+          break;
+          case JSON_ERROR_DEPTH:
+              $jsonError = ' - Maximum stack depth exceeded';
+          break;
+          case JSON_ERROR_STATE_MISMATCH:
+              $jsonError = ' - Underflow or the modes mismatch';
+          break;
+          case JSON_ERROR_CTRL_CHAR:
+              $jsonError = ' - Unexpected control character found';
+          break;
+          case JSON_ERROR_SYNTAX:
+              $jsonError = ' - Syntax error, malformed JSON';
+          break;
+          case JSON_ERROR_UTF8:
+              $jsonError = ' - Malformed UTF-8 characters, possibly incorrectly encoded';
+          break;
+          default:
+              $jsonError = ' - Unknown error';
+          break;
+        }
+
+      //not a valid json value found
+      if (is_null ($decoded)) 
+      {
+        $response['status'] = array(
+        'type' => 'error',
+        'value' => 'Invalid JSON value found',
+        'error' => $jsonError
+        );
+      }
+      else  //valid json values found
+      { 
+          require 'API.php'; //adding API 
+          
+          //check if valid user and return type of user
+          $userSearchResult = TESTsearchTalent();
+          if($userSearchResult > 0) //valid staff user
+          {
+            $responseArray = array(
+                "message" => "successfully retrieved information",
+                "responseType" => "1",
+                "data" =>  $userSearchResult,
+              );
+          }
+          else if($userSearchResult == 0) //database not responding
+          {
+              $responseArray = array(
+                "message"   => "database not responding",
+                "responseType"  => $userSearchResult,
+              );
+          }
+          else if($userSearchResult == -1) //error retrieving information
+          {
+              $responseArray = array(
+                "message"   => "error retrieving information",
+                "responseType"  => $userSearchResult,
+              );
+          }
+
+          /* 
+            reponse returns the following:
+              userdata   successfully retrieved information
+              0   database not responding
+              -1  error retrieving information
+          */
+          $response['results'] = $responseArray; //sending reply
+
+      }//eo valid-json
+    }//eo valid-data
+  }//eo valid - POST
+  else 
+  {
+    $response['status'] = array(
+        'type' => 'error',
+        'value' => 'No JSON value set',
+    );
+  } 
+
+  //responding back to sender
+  $encoded = json_encode($response);
+
+  /* saving incoming file */
+  // Write the contents back to the file
+  $filename = 'test/TESTsearchTalentResponse.json';
+  file_put_contents($filename, var_export($encoded, true));
+
+  $filename = 'test/TESTsearchTalentIncomingData.json';
+  file_put_contents($filename, var_export($decoded, true));
+
+  header  ('Content-type: application/json');
+  exit  ($encoded);
