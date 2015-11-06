1d20fc4b853f6a5682650a4feff7b29508b96780
Luis Castillo <lcast053@0fiu.edu>
2015-11-06 12:55:40 -0500

    adding change password shell script

---
+++ Database/changeDBPassword.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+echo "changing mysql password to fall2015"
+
+#FORMAT:
+#mysqladmin -u root -pcurrentpassword password 'newpassword'
+
+mysqladmin -u root -pkGfEkcmvtW password 'fall2015'
