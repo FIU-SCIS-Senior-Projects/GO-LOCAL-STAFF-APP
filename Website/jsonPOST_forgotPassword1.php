a4103605d7911abe55db175c9f02cbdccf4d6037
luandrecast <luoandre29@yahoo.com>
2015-12-03 13:40:38 -0500

    update forgot password

--- Website/jsonPOST_forgotPassword1.php
+++ Website/jsonPOST_forgotPassword1.php
@@ -1,3 +1,50 @@
+<?php
+
+//making sure its  valid POST request
+if( $_SERVER["REQUEST_METHOD"] == "POST" )
+{
+  //getting php post body data
+  $jsondata = file_get_contents('php://input');
+  if ($jsondata) 
+  {
+    //converting json to php array
+    $decoded = json_decode($jsondata, true);
+    switch (json_last_error())
+    {
+      case JSON_ERROR_NONE:
+      $jsonError = ' - No errors';
+      break;
+      case JSON_ERROR_DEPTH:
+      $jsonError = ' - Maximum stack depth exceeded';
+      break;
+      case JSON_ERROR_STATE_MISMATCH:
+      $jsonError = ' - Underflow or the modes mismatch';
+      break;
+      case JSON_ERROR_CTRL_CHAR:
+      $jsonError = ' - Unexpected control character found';
+      break;
+      case JSON_ERROR_SYNTAX:
+      $jsonError = ' - Syntax error, malformed JSON';
+      break;
+      case JSON_ERROR_UTF8:
+      $jsonError = ' - Malformed UTF-8 characters, possibly incorrectly encoded';
+      break;
+      default:
+      $jsonError = ' - Unknown error';
+      break;
+    }
+
+    //not a valid json value found
+    if (is_null ($decoded)) 
+    {
+      $response['status'] = array(
+        'type' => 'error',
+        'value' => 'Invalid JSON value found',
+        'error' => $jsonError
+        );
+    }
+    else  //valid json values found
+    {
       require 'API.php';/* adding API */
 
       $peopleID;
@@ -66,3 +113,21 @@
               -2  Unable to save changes to Database
         */
         $response['results'] = $responseArray; //sending reply
+
+    }//eo valid-json
+  }//eo valid-data
+}//eo valid - POST
+else 
+{
+  $response['status'] = array(
+    'type' => 'error',
+    'value' => 'No JSON value set',
+    );
+} 
+
+//responding back to sender
+$encoded = json_encode($response);
+
+
+header  ('Content-type: application/json');
+exit  ($encoded);
